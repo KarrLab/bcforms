5fb49317fced1e42b6819dc7663e3a848f32b136
xzheng902 <xzheng20@colby.edu>
2019-06-30 19:07:47 -0400

    BcForm class structure and tests (make sure format is right)

--- bcforms/core.py
+++ bcforms/core.py
@@ -1,47 +1,93 @@
 """ bcforms
 
+:Author: Mike Zheng <xzheng20@colby.edu>
 :Author: Jonathan Karr <karr@mssm.edu>
 :Date: 2019-06-25
 :Copyright: 2019, Karr Lab
 :License: MIT
 """
 
+import lark
 
-class ExampleClass(object):
-    """ Descipton of ExampleClass
+class BcForm(object):
+    """ Biocomplex form
 
     Attributes:
-        attr_1 (:obj:`type of attr_1`): description of attr_1
-        attr_2 (:obj:`type of attr_2`): description of attr_2
-        ...
+        subunits (:obj:`list`): subunits composition of the Biocomplex
+        crosslinks (:obj:`list`): crosslinks in the Biocomplex
+
     """
 
-    def __init__(self, arg_1, arg_2, kwarg_1=None, kwarg_2=None):
+    def __init__(self, subunits=[], crosslinks=[]):
         """
-        Args:
-            arg_1 (:obj:`type of arg_1`): description of arg_1
-            arg_2 (:obj:`type of arg_2`): description of arg_2
-            kwarg_1 (:obj:`type of kwarg_1`, optional): description of kwarg_1
-            kwarg_2 (:obj:`type of kwarg_2`, optional): description of kwarg_2
-            ...
-        """
-        self.attr_1 = arg_1
-        self.attr_2 = arg_2
-
-    def method_1(self, arg_1, arg_2, kwarg_1=None, kwarg_2=None):
-        """ Description of method_1
 
         Args:
-            arg_1 (:obj:`type of arg_1`): description of arg_1
-            arg_2 (:obj:`type of arg_2`): description of arg_2
-            kwarg_1 (:obj:`type of kwarg_1`, optional): description of kwarg_1
-            kwarg_2 (:obj:`type of kwarg_2`, optional): description of kwarg_2
-            ...
+            subunits (:obj:`list`): subunits composition of the Biocomplex
+            crosslinks (:obj:`list`): crosslinks in the Biocomplex
+
+            _parser (:obj:`lark.Lark`): lark grammar parser used in from_str
+        """
+        self.subunits = subunits
+        self.crosslinks = crosslinks
+
+    @property
+    def subunits(self):
+        """ get the subunits of BcForm
 
         Returns:
-            :obj:`type of return value`: description of return value
+            :obj:`list`: subunits
+
+        """
+        return self._subunits
+
+    @subunits.setter
+    def subunits(self, value):
+        """ set the subunits of BcForm
+
+        Args:
+            :obj:`list`: subunits
 
         Raises:
-            :obj:`type of raised exception(s)`: description of raised exceptions
+            :obj:`ValueError`: if `value` is not an instance of `list`
+
+        """
+        if not isinstance(value, list):
+            raise ValueError('`value` must be an instance of `list`')
+        self._subunits = value
+
+    @property
+    def crosslinks(self):
+        """ get the crosslinks of BcForm
+
+        Returns:
+            :obj:`list`: crosslinks
+
+        """
+        return self._crosslinks
+
+    @crosslinks.setter
+    def crosslinks(self, value):
+        """ set the crosslinks of BcForm
+
+        Args:
+            :obj:`list`: crosslinks
+
+        Raises:
+            :obj:`ValueError`: if `value` is not an instance of `list`
+
+        """
+        if not isinstance(value, list):
+            raise ValueError('`value` must be an instance of `list`')
+        self._crosslinks = value
+
+
+    def from_str(self, string):
+        """ create a Biocomplex from a string representation
+
+        Args:
+            string: (:obj:`str`): string representation of a Biocomplex
+
+        Returns:
+            :obj:`BcForm`: the BcForm object of the string
         """
         pass
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,7 @@
 """ Test of bcforms.core
 
-:Author: Name <email>
+:Author: Mike Zheng <xzheng20@colby.edu>
+:Author: Jonathan Karr <karr@mssm.edu>
 :Date: 2019-6-25
 :Copyright: 2019, Karr Lab
 :License: MIT
@@ -10,8 +11,27 @@ from bcforms import core
 import unittest
 
 
-class CoreTestCase(unittest.TestCase):
+class BcFormTestCase(unittest.TestCase):
 
-    def test_1(self):
-        # Example test
-        self.assertTrue(True)  # example assertion
+    def test_init(self):
+        bc_form = core.BcForm()
+        self.assertEqual(bc_form.subunits, [])
+        self.assertEqual(bc_form.crosslinks, [])
+
+    def test_set_subunits(self):
+        bc_form = core.BcForm()
+
+        bc_form.subunits = []
+
+        with self.assertRaises(ValueError):
+            bc_form.subunits = None
+
+
+    def test_set_crosslinks(self):
+
+        bc_form = core.BcForm()
+
+        bc_form.crosslinks = []
+
+        with self.assertRaises(ValueError):
+            bc_form.crosslinks = None

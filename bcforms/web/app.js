186d5eb91e53ccc6bd31da4a062816a968b2fdfb
xzheng902 <xzheng20@colby.edu>
2019-07-23 14:27:11 -0400

    webform skeleton for testing

--- bcforms/web/app.js
+++ bcforms/web/app.js
@@ -1,1 +1,27 @@
-// todo
+$(document).foundation()
+
+$('#submit').click(function (evt) {
+    bc_form = $("#bc_form_in").val().trim()
+
+    if (bc_form == null || bc_form == '') {
+        return;
+    }
+
+    data = {
+        'form': bc_form
+    }
+
+    $.ajax({
+      type: 'post',
+      url: '/api/bcform/',
+      data: JSON.stringify(data),
+      contentType : 'application/json',
+      dataType: 'json',
+      success: set_properties
+    })
+
+})
+
+set_properties = function(data, status, jqXHR) {
+    $("#output_test").val(status)
+}

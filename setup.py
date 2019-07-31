b32f8a3bc1b2bc6180f464dcd305e7d2bcedb612
Jonathan Karr <jonrkarr@gmail.com>
2019-07-31 16:23:03 -0400

    changing contact email to info@karrlab.org

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
     url="https://github.com/KarrLab/" + name,
     download_url='https://github.com/KarrLab/' + name,
     author="Karr Lab",
-    author_email="karr@mssm.com",
+    author_email="info@karrlab.org",
     license="MIT",
     keywords='complex post-translational modification crosslinks systems biology',
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),

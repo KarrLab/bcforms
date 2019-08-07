bc5ca8360e3846686761e0c1ec7aea2b1ef53b2f
xzheng902 <xzheng20@colby.edu>
2019-08-07 10:21:29 -0400

    add l/r_monomer_alphabet to xlink xlsx and yml

--- bcforms/xlink/xlink_xlsx2yml.py
+++ bcforms/xlink/xlink_xlsx2yml.py
@@ -25,13 +25,15 @@ def main():
 
         row_dict['synonyms'] = []
         row_dict['synonyms'].append(row['resid_name'])
-        
+
         if not pd.isnull(row['bond_common_name']):
             row_dict['common_name'] = row['bond_common_name']
 
+        row_dict['l_monomer_alphabet'] = row['left_monomer_alphabet']
         row_dict['l_monomer'] = row['left_monomer']
         row_dict['l_bond_atoms'] = [atom.strip() for atom in row['left_bond_atoms'].split(',')]
         row_dict['l_displaced_atoms'] = [atom.strip() for atom in row['left_displace_atoms'].split(',')]
+        row_dict['r_monomer_alphabet'] = row['right_monomer_alphabet']
         row_dict['r_monomer'] = row['right_monomer']
         row_dict['r_bond_atoms'] = [atom.strip() for atom in row['right_bond_atoms'].split(',')]
         row_dict['r_displaced_atoms'] = [atom.strip() for atom in row['right_displace_atoms'].split(',')]
